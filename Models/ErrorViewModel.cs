839c96c4af04e83be2760cb5115b4b1adb579d24
Murat SELEN <7muratselen@gmail.com>
2024-03-28 23:42:54 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SELENAVM04.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
